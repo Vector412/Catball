2d53cd490ae2aa3d7da59cebae7c0bfe2376af84
SashaV <43315310+sayonarabyee@users.noreply.github.com>
2020-11-23 14:44:02 +0300

    Score mechanic

---
+++ CatBallGame/Assets/Scripts/Player.cs
@@ -0,0 +1,11 @@
+﻿public class Player : Singleton<Player>
+{
+	private static int currentCoins;
+	public delegate void OnCoinsChanged(int coins);
+	public event OnCoinsChanged onCoinTake;
+	public void AddCoin()
+	{
+		currentCoins++;
+		onCoinTake.Invoke(currentCoins);
+	}
+}
---
+++ CatBallGame/Assets/Scripts/ScoreText.cs
@@ -0,0 +1,9 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class ScoreText : MonoBehaviour
+{
+	[SerializeField] TextMeshProUGUI scoreText;
+	private void Start() => Player.Instance.onCoinTake += OnPlayerCoinsChanged;
+	void OnPlayerCoinsChanged(int newCoins) => scoreText.text = "Score: " + newCoins.ToString();
+}
