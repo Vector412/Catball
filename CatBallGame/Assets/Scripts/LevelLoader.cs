aa488b6356789baa58d75da79cf9fd274a445d78
SashaV <43315310+sayonarabyee@users.noreply.github.com>
2020-11-24 12:52:37 +0300

    Load level system

---
+++ CatBallGame/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class LevelLoader : MonoBehaviour
+{
+	[SerializeField] LevelList levels;
+	private static int openedLevel;
+	private GameObject level;
+
+	private void Awake()
+	{
+		DontDestroyOnLoad(gameObject);
+	}
+	private void Start()
+	{
+		if (PlayerPrefs.GetInt("currentOpenedLevel") == 0)
+		{
+			PlayerPrefs.SetInt("currentOpenedLevel", 0);
+			level = levels.levelPrefabs[PlayerPrefs.GetInt("currentOpenedLevel")];
+			CreateLevel(level);
+		}
+		else
+		{
+			openedLevel = PlayerPrefs.GetInt("currentOpenedLevel");
+			level = levels.levelPrefabs[PlayerPrefs.GetInt("currentOpenedLevel")];
+			CreateLevel(level);
+		}
+	}
+	public void LoadNextLevel()
+	{
+		Destroy(level);
+		SaveProgress();
+		level = levels.levelPrefabs[PlayerPrefs.GetInt("currentOpenedLevel")];
+		CreateLevel(level);
+	}
+	public void SaveProgress() => PlayerPrefs.SetInt("currentOpenedLevel", openedLevel++);
+	public void DeleteSaves() => PlayerPrefs.DeleteAll();
+	public void CreateLevel(GameObject levelToCreate)
+	{
+		Instantiate(levelToCreate);
+	}
+}
---
+++ CatBallGame/Assets/Scripts/ScriptableObjects/LevelList.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Levels", menuName = "LevelList")]
+public class LevelList : ScriptableObject
+{
+	public List<GameObject> levelPrefabs = new List<GameObject>();
+}
