0327bf364b335fbf9c077d50acd5e64157980f53
Vector412 <61909904+Vector412@users.noreply.github.com>
2020-11-24 13:35:29 +0300

    Draw line

---
+++ CatBallGame/Assets/Scenes/Scripts/Line.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(PolygonCollider2D))]
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(LineRenderer))]
+[DisallowMultipleComponent]
+public class Line : MonoBehaviour
+{
+	[Range(0, 5000)]
+	public List<Vector2> points;
+	public float maxPoints;
+	[SerializeField] float pointZPosition;
+	[SerializeField] bool autoAddColliderPoint = true;
+	
+	
+	 List<Vector2> polygon2DPoints;
+	 LineRenderer lineRenderer;
+	 PolygonCollider2D polygonCollider2D;
+	 Rigidbody2D rigidBody2D;
+
+	
+	float pointMinOffset = 0.05f;
+	static Vector2 tempVector;
+    static Vector2 direction;
+	static float angle;
+	static float halfWidth;
+
+
+	void Awake ()
+	{
+		points = new List<Vector2> ();
+		polygon2DPoints = new List<Vector2> ();
+		lineRenderer = GetComponent<LineRenderer> ();
+		polygonCollider2D = GetComponent<PolygonCollider2D> ();
+		rigidBody2D = GetComponent<Rigidbody2D> ();
+
+		
+		halfWidth = lineRenderer.endWidth / 2.0f;
+	}
+
+	
+	public void AddPoint (Vector3 point)
+	{
+		if (points.Count > 1) 
+		{
+			if (Vector2.Distance (point, points [points.Count - 1]) < pointMinOffset)
+			{
+				Debug.Log(11);
+				return;
+			}
+		}
+
+		point.z = pointZPosition;
+		points.Add (point);
+        lineRenderer.positionCount++;
+		lineRenderer.SetPosition (lineRenderer.positionCount - 1, point);
+
+		if (autoAddColliderPoint)
+		{
+			AddPointToCollider (points.Count - 1);
+		}
+	}
+
+	
+	public void EnableCollider ()
+	{
+		polygonCollider2D.enabled = true;
+	}
+	
+	
+	public void DisableCollider ()
+	{
+		polygonCollider2D.enabled = false;
+	}
+
+
+	public void SetRigidBodyType(RigidbodyType2D type){
+		rigidBody2D.bodyType = type;
+	}
+
+
+	public void SimulateRigidBody ()
+	{
+		rigidBody2D.simulated = true;
+	}
+
+	
+	public bool ReachedPointsLimit(){
+		return points.Count >= maxPoints;
+	}
+
+	public void AddPointToCollider (int index)
+	{
+		direction = points [index] - points [index + 1 < points.Count ? index + 1 : (index - 1 >= 0 ? index - 1 : index)];
+		angle = Mathf.Atan2 (direction.x, -direction.y);
+
+		tempVector = points [index];
+		tempVector.x = tempVector.x + halfWidth * Mathf.Cos (angle);
+		tempVector.y = tempVector.y + halfWidth * Mathf.Sin (angle);
+		polygon2DPoints.Insert (polygon2DPoints.Count, tempVector);
+
+		tempVector = points [index];
+		tempVector.x = tempVector.x - halfWidth * Mathf.Cos (angle);
+		tempVector.y = tempVector.y - halfWidth * Mathf.Sin (angle);
+		polygon2DPoints.Insert (0, tempVector);
+
+		polygonCollider2D.points = polygon2DPoints.ToArray ();
+	}
+}
---
+++ CatBallGame/Assets/Scenes/Scripts/LineFactory.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+[DisallowMultipleComponent]
+public class LineFactory : MonoBehaviour
+{
+	[SerializeField] GameObject linePrefab;
+	[SerializeField] bool enableLineLife;
+	[SerializeField] bool isRunning;
+	[SerializeField] Line currentLine;
+	[SerializeField] Transform lineParent;
+	[SerializeField] Image lineLife;
+
+	RigidbodyType2D lineRigidBodyType = RigidbodyType2D.Kinematic;
+	LineEnableMode lineEnableMode = LineEnableMode.ON_CREATE;
+	
+	
+	void Update ()
+	{
+		if (Input.GetMouseButtonDown (0))
+		{
+			CreateNewLine ();
+		} 
+		else if (Input.GetMouseButtonUp (0))
+		{
+			RelaseCurrentLine ();
+		}
+
+		if (currentLine != null)
+		{
+			currentLine.AddPoint (Camera.main.ScreenToWorldPoint (Input.mousePosition));
+			UpdateLineLife ();
+			if (currentLine.ReachedPointsLimit())
+			{
+				RelaseCurrentLine ();
+			}
+		}
+	}
+
+	private void CreateNewLine ()
+	{
+		currentLine = Instantiate (linePrefab, Vector3.zero, Quaternion.identity).GetComponent<Line> ();
+		currentLine.transform.SetParent (lineParent);
+		currentLine.SetRigidBodyType (lineRigidBodyType);
+
+		if (lineEnableMode == LineEnableMode.ON_CREATE)
+		{
+			EnableLine ();
+		}
+	}
+
+	private void EnableLine ()
+	{
+		currentLine.EnableCollider ();
+		currentLine.SimulateRigidBody ();
+	}
+
+	private void RelaseCurrentLine ()
+	{
+		if (lineEnableMode == LineEnableMode.ON_RELASE)
+		{
+			EnableLine ();
+		}
+
+		currentLine = null;
+	}
+
+	private void UpdateLineLife ()
+	{
+		if (!enableLineLife)
+		{
+			return;
+		}
+
+		if (lineLife == null)
+		{
+			return;
+		}
+
+		lineLife.fillAmount = 1 - (currentLine.points.Count / currentLine.maxPoints);
+	}
+
+	public enum LineEnableMode
+	{
+		ON_CREATE,
+		ON_RELASE}
+
+	;
+}
